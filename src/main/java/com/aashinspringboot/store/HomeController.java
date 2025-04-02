19477841042888322d9f04295a527580d4cee744
Aashin A P <apaashin@gmail.com>
2025-04-02 17:18:35 +0530

    Made the final changes

--- src/main/java/com/aashinspringboot/store/HomeController.java
+++ src/main/java/com/aashinspringboot/store/HomeController.java
@@ -1,15 +1,18 @@
 package com.aashinspringboot.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index(){
-        //return "index.html";
-        String viewName = getViewName();
-        return viewName;
+        System.out.println("appName: "+appName);
+        return "index.html";
     }
 
     private String getViewName(){
---
+++ src/main/java/com/aashinspringboot/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.aashinspringboot.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
